1aafdaa72bdb6eaf593cd3f5cb798dd6a4bb55f9
Vencel Kátai <vencelster@gmail.com>
2019-05-16 23:21:59 +0200

    Fixing brute force auction manager

--- Vatera/Implementation/AuctionManager/BruteForceAuctionManager.cs
+++ Vatera/Implementation/AuctionManager/BruteForceAuctionManager.cs
@@ -11,20 +11,20 @@ namespace Vatera.Implementation.AuctionManager
         {
             LinkedListOrderStore successfulOrders = null;
 
-            for (int i = 1; i <= Math.Pow(2, product.Orders.Count); ++i)
+            for (int i = 1; i < Math.Pow(2, product.Orders.Count); ++i)
             {
                 LinkedListOrderStore betterCombination = new LinkedListOrderStore();
 
                 int j = 0;
                 foreach (var order in product.Orders)
                 {
-                    if (i / Math.Pow(2, j++) % 2 == 1)
+                    if ((int)(i / Math.Pow(2, j++)) % 2 == 1)
                     {
                         betterCombination.Insert(order);
                     }
                 }
 
-                if (_sumRatings(betterCombination) <= product.Count && _sumOrderCount(betterCombination) > _sumOrderCount(successfulOrders))
+                if (_sumOrderCount(betterCombination) <= product.Count && _sumRatings(betterCombination) > _sumRatings(successfulOrders))
                 {
                     successfulOrders = betterCombination;
                 }
@@ -35,16 +35,16 @@ namespace Vatera.Implementation.AuctionManager
 
         private int _sumRatings(LinkedListOrderStore orders)
         {
-            return orders.Sum(order => order.Customer.Rating);
-        }
-
-        private int _sumOrderCount(LinkedListOrderStore orders)
-        {
             if (orders == null)
             {
                 return 0;
             }
 
+            return orders.Sum(order => order.Customer.Rating);
+        }
+
+        private int _sumOrderCount(LinkedListOrderStore orders)
+        {
             return orders.Sum(order => order.Count);
         }
     }
