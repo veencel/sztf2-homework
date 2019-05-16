1cc0609e6bf6cdc78dd6b6c5883f2aad5bdfda01
Vencel Kátai <vencelster@gmail.com>
2019-05-16 21:12:01 +0200

    Implementing AuctionManager with the backtrack algorithm

---
+++ Vatera/Filters/FailedOrderFilter.cs
@@ -0,0 +1,23 @@
+﻿using Vatera.Implementation;
+using Vatera.Interfaces;
+
+namespace Vatera.Filters
+{
+    static class FailedOrderFilter
+    {
+        public static IOrderStore Filter(IOrderStore orders, IOrderStore successfulOrders)
+        {
+            IOrderStore failedOrders = new LinkedListOrderStore();
+
+            foreach (var order in orders)
+            {
+                if (!successfulOrders.Contains(order))
+                {
+                    failedOrders.Insert(order);
+                }
+            }
+
+            return failedOrders;
+        }
+    }
+}
---
+++ Vatera/Implementation/AuctionManager/BackTrackAuctionManager.cs
@@ -0,0 +1,54 @@
+﻿using Vatera.Filters;
+using Vatera.Interfaces;
+using Vatera.Models;
+
+namespace Vatera.Implementation.AuctionManager
+{
+    public class BacktrackAuctionManager: IAuctionManager
+    {
+        public AuctionResult FinishAuction(Product product)
+        {
+            IOrderStore temp = new LinkedListOrderStore();
+            IOrderStore successfulOrders = new LinkedListOrderStore();
+
+            _backtrack(1, ref temp, ref successfulOrders, product);
+
+            return new AuctionResult(product, successfulOrders, FailedOrderFilter.Filter(product.Orders, successfulOrders));
+        }
+
+        private void _backtrack(int level, ref IOrderStore temp, ref IOrderStore successfulOrders, Product product)
+        {
+            for (int i = 0; i <= 1; ++i)
+            {
+                if (i == 0)
+                {
+                    temp.Insert(product.Orders.Get(level));
+                }
+                else
+                {
+                    temp.Remove(product.Orders.Get(level));
+                }
+
+                if (_isPossibleSolution(level, temp, product))
+                {
+                    if (level == product.Orders.Count)
+                    {
+                        if (temp.Sum(order => order.Customer.Rating) > successfulOrders.Sum(order => order.Customer.Rating))
+                        {
+                            successfulOrders = new LinkedListOrderStore(temp);
+                        }
+                    }
+                    else
+                    {
+                        _backtrack(level + 1, ref temp, ref successfulOrders, product);
+                    }
+                }
+            }
+        }
+
+        private bool _isPossibleSolution(int level, IOrderStore orders, Product product)
+        {
+            return orders.Sum(order => order.Count) <= product.Count;
+        }
+    }
+}
