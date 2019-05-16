2b6f4896d89aaa680bd1ef4699053d0e4481d210
Vencel Kátai <vencelster@gmail.com>
2019-05-16 21:12:31 +0200

    Implementing AuctionManager with the brute force algorithm

---
+++ Vatera/Implementation/AuctionManager/BruteForceAuctionManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Vatera.Filters;
+using Vatera.Interfaces;
+using Vatera.Models;
+
+namespace Vatera.Implementation.AuctionManager
+{
+    public class BruteForceAuctionManager: IAuctionManager
+    {
+        public AuctionResult FinishAuction(Product product)
+        {
+            LinkedListOrderStore successfulOrders = null;
+
+            for (int i = 1; i <= Math.Pow(2, product.Orders.Count); ++i)
+            {
+                LinkedListOrderStore betterCombination = new LinkedListOrderStore();
+
+                int j = 0;
+                foreach (var order in product.Orders)
+                {
+                    if (i / Math.Pow(2, j++) % 2 == 1)
+                    {
+                        betterCombination.Insert(order);
+                    }
+                }
+
+                if (_sumRatings(betterCombination) <= product.Count && _sumOrderCount(betterCombination) > _sumOrderCount(successfulOrders))
+                {
+                    successfulOrders = betterCombination;
+                }
+            }
+
+            return new AuctionResult(product, successfulOrders, FailedOrderFilter.Filter(product.Orders, successfulOrders));
+        }
+
+        private int _sumRatings(LinkedListOrderStore orders)
+        {
+            return orders.Sum(order => order.Customer.Rating);
+        }
+
+        private int _sumOrderCount(LinkedListOrderStore orders)
+        {
+            if (orders == null)
+            {
+                return 0;
+            }
+
+            return orders.Sum(order => order.Count);
+        }
+    }
+}
